e121c7c0a26882cf2c77dde1d5e250ac22fed847
diardomarendikrista <diardomarendikrista@gmail.com>
2021-01-28 18:08:00 +0700

    backend

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const router = require("./routes/index");
 const app = express();
 const port = 3000;
 
-const gmapMarker = require('./helpers/gmapMarker');
-app.locals.gmapMarker = gmapMarker;
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/css", express.static("public"));
